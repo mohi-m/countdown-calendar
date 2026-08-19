c664eaaa83e185a8e298173aaf31b92cb4e23beb
mohi-m <mohi25081998@gmail.com>
2026-08-19 07:47:59 -0400

    spell check

--- src/memories.ts
+++ src/memories.ts
@@ -63,7 +63,7 @@ const days = [
   ['2026-08-16', 'Popcorn monster', 'Isko dekhke I be like, This is definetly a baby in a girls body.'],
   ['2026-08-17', 'Almost there', 'I can already smell the beaches and splashing you with salty water and then eating some of those good good cheesey fries.'],
   ['2026-08-18', 'Three days', 'We both smol babies in this big big world. But I am so glad we are smol babies together.'],
-  ['2026-08-19', 'Two sides', 'Hotty booby and goofy momo. When did we decide to pick sides. But I think its more like I can be hottie so I just pick the goofy side.'],
+  ['2026-08-19', 'Two sides', 'Hotty booby and goofy momo. When did we decide to pick sides. But I think its more like I cant be hottie so I just pick the goofy side.'],
   ['2026-08-20', 'Tomorrow', 'One last sleep with a continent in the middle.'],
   ['2026-08-21', 'Today', 'No more countdown. Tonight, I am coming home to you.'],
 ] as const
